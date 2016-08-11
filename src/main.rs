9a9a172aa0cd9cc7dd4f93e73d570fa2b6f6b603
Lucas <lucas@agtlucas.com>
2016-08-11 19:54:22 -0300

    Do not allow letters

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
     io::stdin().read_line(&mut guess)
     .expect("Failed to read line.");
 
-    let guess: u32 = guess.trim().parse() {
+    let guess: u32 = match guess.trim().parse() {
       Ok(num) => num,
       Err(_) => continue,
     };
@@ -35,5 +35,4 @@ fn main() {
       }
     }
   }
-
 }
